336b1f2a1ba6eb76eefb541c78b8daaf8c58de1a
armando.assini <arm.assini@gmail.com>
2023-07-31 14:47:29 -0300

    findFirstMostFrequent Funcional

--- src/findFirstMostFrequent.ts
+++ src/findFirstMostFrequent.ts
@@ -15,5 +15,16 @@ interface MostFrequentType {
 export const findFirstMostFrequent = (
   inputArray: Array<string | number>,
 ): MostFrequentType => {
-  return { value: '', occurrences: 0 };
+  const auxCount = {};
+
+  for (const num of inputArray) {
+    auxCount[num] = auxCount[num] ? auxCount[num] + 1 : 1;
+  }
+
+  let maxOccur: number = Object.values(auxCount).reduce((a, b) => a > b ? a : b) as number;
+  let maxValue: string | number = Object.keys(auxCount).find(key => auxCount[key] === maxOccur,);
+
+  if (parseInt(maxValue)) {
+    return { value: parseInt(maxValue), occurrences: maxOccur };
+  } else return { value: maxValue, occurrences: maxOccur };
 };
