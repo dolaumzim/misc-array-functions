e2ac0cda8bcf0faf0ed5a49ec1d8daef1816b2c8
armando.assini <arm.assini@gmail.com>
2023-07-31 11:24:28 -0300

    arrayClone Funcional

--- src/arrayClone.ts
+++ src/arrayClone.ts
@@ -5,5 +5,9 @@
  * @param {T[]} arr
  */
 export const arrayClone = <T>(arr: T[]): T[] => {
-  return [];
+  let result : any = new Array();
+  for (let i=0; i<arr.length; i++){
+  result.push(arr[i]);
+}
+  return result;
 };
