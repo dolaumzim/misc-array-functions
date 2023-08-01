899dd48d8e2f160b876f370955da2bdc5ff0f2f6
armando.assini <arm.assini@gmail.com>
2023-08-01 08:20:05 -0300

    arrayClone Linted

--- src/arrayClone.ts
+++ src/arrayClone.ts
@@ -5,9 +5,9 @@
  * @param {T[]} arr
  */
 export const arrayClone = <T>(arr: T[]): T[] => {
-  let result : any = new Array();
-  for (let i=0; i<arr.length; i++){
-  result.push(arr[i]);
-}
+  let result: any = new Array();
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]);
+  }
   return result;
 };
