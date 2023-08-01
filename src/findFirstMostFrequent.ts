641a61c40669a3975ed350083dcb0b45fab107d4
armando.assini <arm.assini@gmail.com>
2023-08-01 08:20:23 -0300

    findFirstMostFrequent Linted

--- src/findFirstMostFrequent.ts
+++ src/findFirstMostFrequent.ts
@@ -21,8 +21,12 @@ export const findFirstMostFrequent = (
     auxCount[num] = auxCount[num] ? auxCount[num] + 1 : 1;
   }
 
-  let maxOccur: number = Object.values(auxCount).reduce((a, b) => a > b ? a : b) as number;
-  let maxValue: string | number = Object.keys(auxCount).find(key => auxCount[key] === maxOccur,);
+  let maxOccur: number = Object.values(auxCount).reduce((a, b) =>
+    a > b ? a : b,
+  ) as number;
+  let maxValue: string | number = Object.keys(auxCount).find(
+    key => auxCount[key] === maxOccur,
+  );
 
   if (parseInt(maxValue)) {
     return { value: parseInt(maxValue), occurrences: maxOccur };
